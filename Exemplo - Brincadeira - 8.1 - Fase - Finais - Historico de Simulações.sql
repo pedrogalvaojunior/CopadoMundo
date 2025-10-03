b2bd16dd75d1e7e75de98a9cb8ebf248a5d266ea
Junior Galvão MVP <pedrogalvaojunior@gmail.com>
2025-10-03 19:50:44 -0300

    Update Exemplo - Brincadeira - 8.1 - Fase - Finais - Historico de Simulações.sql

--- Exemplo - Brincadeira - 8.1 - Fase - Finais - Historico de Simulações.sql
+++ Exemplo - Brincadeira - 8.1 - Fase - Finais - Historico de Simulações.sql
@@ -1,3 +1,7 @@
+-- Acessando --
+Use Qatar2022
+Go
+
 -- Criando a Stored Procedure P_HistoricoSimulacao --
 Create Or Alter Procedure P_HistoricoSimulacao @CodigoSimulador Char(1)
 As
@@ -6,24 +10,24 @@ Begin
 -- Desativando a Contagem de Linhas --
 Set NoCount On
 
--- Armazenando o Histórico de Jogos realizados --
+-- Armazenando o HistÃ³rico de Jogos realizados --
 Insert Into HistoricoJogos (CodigoSimulador, CodigoJogo, CodigoGrupoSorteio, CodigoSelecao1, CodigoSelecao2, CodigoJogoNoGrupo, GolsSelecao1, 
                                            GolsSelecao2, CartoesAmareloSelecao1, CartoesAmareloSelecao2,  CartoesVermelhoSelecao1, CartoesVermelhoSelecao2)
 Select @CodigoSimulador, CodigoJogo, CodigoGrupoSorteio, CodigoSelecao1, CodigoSelecao2, CodigoJogoNoGrupo, GolsSelecao1, 
                                            GolsSelecao2, CartoesAmareloSelecao1, CartoesAmareloSelecao2,  CartoesVermelhoSelecao1, CartoesVermelhoSelecao2 
 From Jogos
 
--- Armazenado o Histórico da última Classificação Geral --
+-- Armazenado o HistÃ³rico da Ãºltima ClassificaÃ§Ã£o Geral --
 Insert Into HistoricoClassificacaoGeral (CodigoSimulador, CodigoClassificacaoGeral, PosicaoClassificacaoGeral, CodigoSelecao, Jogos, 
                                                                  Pontos, Vitorias, Empates, Derrotas, CartoesAmarelos, CartoesVermelhos, GolsPro, GolsContra, SaldoGols)
 Select @CodigoSimulador, CodigoClassificacaoGeral, PosicaoClassificacaoGeral, CodigoSelecao, Jogos, 
            Pontos, Vitorias, Empates, Derrotas, CartoesAmarelos, CartoesVermelhos, GolsPro, GolsContra, SaldoGols
 From ClassificacaoGeral
 
--- Armazenado o Historico de Simulações realizadas --
+-- Armazenado o Historico de SimulaÃ§Ãµes realizadas --
 Insert Into HistoricoSimulacao (CodigoSimulador, Jogo, Placar, SelecaoCampea, SelecaoVice)
 Select @CodigoSimulador,
-           Concat('Jogo nº ',J.CodigoJogo,' - ', S1.NomeSelecao,' x ',S2.NomeSelecao) As 'Final',
+           Concat('Jogo nÂº ',J.CodigoJogo,' - ', S1.NomeSelecao,' x ',S2.NomeSelecao) As 'Final',
            Concat(S1.NomeSelecao,' ',J.GolsSelecao1,' x ',J.GolsSelecao2, ' ',S2.NomeSelecao) As Placar,
 		   Case When J.GolsSelecao1 > J.GolsSelecao2 Then S1.NomeSelecao Else S2.NomeSelecao End As 'World Champions',
 		   Case When J.GolsSelecao1 > J.GolsSelecao2 Then S2.NomeSelecao Else S1.NomeSelecao End As 'Runner-up'
@@ -36,3 +40,4 @@ From Jogos J Inner Join Grupos G
 Where J.CodigoJogo = 64
 End
 Go
+
